bdd0bbe4ccf5edcdb1918de50b42dec10cb67c72
geetagalaxyadvertising2002 <geetagalaxyadvertising2002@gmail.com>
2026-05-28 02:46:38 -0700

    yvvh

--- project/settings.py
+++ project/settings.py
@@ -167,7 +167,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:50625",
+    "http://localhost:54178",
     "https://magic-salon-admin.netlify.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
