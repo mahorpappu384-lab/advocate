55a8b2e2e257178000500bfe9838a9f0ef69b385
geetagalaxyadvertising2002 <geetagalaxyadvertising2002@gmail.com>
2026-06-16 08:44:58 -0700

    bn

--- api/models.py
+++ api/models.py
@@ -54,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     phone = PhoneNumberField(blank=True, null=True, unique=True)
     full_name = models.CharField(max_length=150)
 
